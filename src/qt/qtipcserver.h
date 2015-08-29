64082569d9036e15df2a689e0163b08c656c7b4c
Sect.48 <Sect.48>
2015-08-29 17:56:07 +0100

    Sect48
    sc

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define sector-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "sectorURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
